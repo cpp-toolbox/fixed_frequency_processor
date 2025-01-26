31c11b22fa509d5cab46613d76094a91ff283edd
cuppajoeman <ccn@cuppajoeman.com>
2025-01-25 21:47:53 -0500

    feat: initial logic

---
+++ fixed_frequency_processor.cpp
@@ -0,0 +1,50 @@
+#include "fixed_frequency_processor.hpp"
+
+FixedFrequencyProcessor::FixedFrequencyProcessor(int frequency, std::function<void(int, double)> process_fun)
+    : process_fun(process_fun), periodic_signal(frequency) {}
+
+void FixedFrequencyProcessor::add_id(int id) {
+    std::cout << "FFP Adding ID: " << id << std::endl;
+    ids_to_process.push_back(id);
+}
+
+void FixedFrequencyProcessor::add_ids(std::vector<int> ids) {
+    for (const auto id : ids) {
+        this->add_id(id);
+    }
+}
+
+bool FixedFrequencyProcessor::attempt_to_process() {
+    if (periodic_signal.process_and_get_signal()) {
+        unsigned int count = 0;
+        while (!ids_to_process.empty()) {
+            int id = ids_to_process.front();
+            ids_to_process.erase(ids_to_process.begin());
+            process_fun(id, periodic_signal.get_last_delta_time());
+            processed_ids.push_back(id);
+            count++;
+        }
+
+        if (count >= 1 and ids_to_process.empty()) {
+            std::cout << "FFP: All IDs processed." << std::endl;
+            processed_at_least_one_id = true;
+        }
+
+        return true;
+    }
+    return false;
+}
+int FixedFrequencyProcessor::get_last_processed_id() const {
+    if (processed_ids.empty()) {
+        throw std::out_of_range("No IDs have been processed yet.");
+    }
+    return processed_ids.back(); // Returns the last element
+}
+
+void FixedFrequencyProcessor::display_processed_ids() const {
+    std::cout << "FFP Processed IDs: ";
+    for (const auto &id : processed_ids) {
+        std::cout << id << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ fixed_frequency_processor.hpp
@@ -0,0 +1,54 @@
+#ifndef FIXED_FREQUENCY_PROCESSOR_HPP
+#define FIXED_FREQUENCY_PROCESSOR_HPP
+
+#include <vector>
+#include <functional>
+#include <iostream>
+#include "../periodic_signal/periodic_signal.hpp"
+
+/**
+ * @brief A class for processing IDs at a fixed frequency.
+ */
+class FixedFrequencyProcessor {
+  private:
+    std::vector<int> processed_ids;               ///< Vector of IDs that have been processed.
+    std::vector<int> ids_to_process;              ///< Vector of IDs that need to be processed.
+    std::function<void(int, double)> process_fun; ///< Function to process each ID.
+    PeriodicSignal periodic_signal;
+
+  public:
+    bool processed_at_least_one_id = false;
+    /**
+     * @brief Constructs a FixedFrequencyProcessor.
+     * @param frequency The number of times the process should happen per second.
+     * @param fun Function to process each ID.
+     */
+    FixedFrequencyProcessor(int frequency, std::function<void(int, double)> process_fun);
+
+    int get_last_processed_id() const;
+
+    /**
+     * @brief Adds an ID to the list of IDs to be processed.
+     * @param id The ID to add.
+     */
+    void add_id(int id);
+
+    /**
+     * @brief Adds a list of IDs to the list of IDs to be processed.
+     * @param ids A vector of IDs to add.
+     */
+    void add_ids(std::vector<int> ids);
+
+    /**
+     * @brief Attempts to process IDs if the accumulated time exceeds the processing interval.
+     * @return true if processing was done; false otherwise.
+     */
+    bool attempt_to_process();
+
+    /**
+     * @brief Displays the list of processed IDs.
+     */
+    void display_processed_ids() const;
+};
+
+#endif // FIXED_FREQUENCY_PROCESSOR_HPP
